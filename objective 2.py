2a8affe50f15e31c61635bf391ece4b2c95376cf
aisyah2521 <s22a0046@siswa.umk.edu.my>
2025-11-02 17:40:09 +0800

    Update objective 2.py

--- objective 2.py
+++ objective 2.py
@@ -3,11 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 st.set_page_config(
-  page_title="Objective 1"
+  page_title="Objective 2"
 )
-st.header("Objective 1", divider="gray")
+st.header("Objective 2", divider="gray")
 
 
 # The raw GitHub URL for the CSV file
@@ -53,50 +54,9 @@ else:
     st.error("The DataFrame is empty. Please check the URL or file format.")
 
 fig, ax = plt.subplots(figsize=(8, 6))
-ax.hist(df['Age_of_Participants'], bins=20, edgecolor='black')
-ax.set_title('Distribution of Age of Participants')
-ax.set_xlabel('Age')
-ax.set_ylabel('Frequency')
+sns.kdeplot(df['Physical_Health_Summary'].dropna(), fill=True, ax=ax)
+ax.set_title('Density Plot of Physical Health Summary')
+ax.set_xlabel('Physical Health Summary Score')
+ax.set_ylabel('Density')
 
 st.pyplot(fig)
-
-"""
-  **Figure 1** This graph is a **histogram** that shows the Distribution of Age Of Participants. 
-  """
-
-gender_counts = df['Gender'].value_counts()
-
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.pie(
-    gender_counts,
-    labels=gender_counts.index,
-    autopct='%1.1f%%',
-    startangle=90,
-    colors=['skyblue', 'lightcoral']
-)
-ax.set_title('Distribution of Gender')
-ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-st.pyplot(fig)
-
-"""
-  **Figure 2** This graph is a **pie chart** title "Distribution Of Gender."
-  """
-
-
-fig, ax = plt.subplots(figsize=(10, 6))
-df['Frequency_of_Blood_Transfusion'].value_counts().plot(
-    kind='bar', edgecolor='black', ax=ax
-)
-ax.set_title('Distribution of Frequency of Blood Transfusion')
-ax.set_xlabel('Frequency of Blood Transfusion')
-ax.set_ylabel('Count')
-ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
-plt.tight_layout()
-
-st.pyplot(fig)
-
-"""
-  **Figure 3** This graph is a **bar chart** title "Distribution Of Frequency Of Blood Transfusion".It compares
-  the number of participants (count) falling into three diffrent categories of transfusion frequency.
-  """
