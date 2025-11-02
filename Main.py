56e8708abc8ddfb8365d2b14a45b151f7c15cc8e
aisyah2521 <s22a0046@siswa.umk.edu.my>
2025-11-02 17:31:14 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ home = st.Page('objective 2.py', title='Homepage', default=True, icon=":material
 
 pg = st.navigation(
     {
-        "Menu": [Objective 1, Objective 2]
+        "Menu": [Objective1, Objective2]
     }
 )
 
