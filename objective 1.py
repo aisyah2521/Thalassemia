bdc75a57bce9ebcd7303987884010bc81e03c989
aisyah2521 <s22a0046@siswa.umk.edu.my>
2025-11-02 23:13:17 +0800

    Update objective 1.py

--- objective 1.py
+++ objective 1.py
@@ -11,7 +11,7 @@ st.header("Objective 1", divider="gray")
 
 
 # The raw GitHub URL for the CSV file
-DATA_URL = "https://raw.githubusercontent.com/aisyah2521/Thalassemia/refs/heads/main/cleaned_thalassemia_qol_data%20(1).csv"
+DATA_URL = "https://raw.githubusercontent.com/aisyah2521/Thalassemia/refs/heads/main/thalassemia_qol_data%20(1).csv"
 
 """
   **Thalassemia** This dataset presents comprehensive health-related quality of life (HRQoL) metrics collected from thalassemia patients in Bangladesh using 
